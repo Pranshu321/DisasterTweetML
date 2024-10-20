3f87ed93bd15e6788b3013fce392d2349f589c9a
Pranshu321 <pranshujain0111@gmail.com>
2024-10-20 11:22:18 +0530

    Refactor code to use GridSearchCV for hyperparameter tuning in Logistic Regression and SVM models

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+tfidf = pickle.load(open('tfidf_vectorizer.pk1', 'rb'))
+
+model = pickle.load(open('model.pk1', 'rb'))
+
+def predict_the_output(text):
+    text = [text]
+    text = tfidf.transform(text)
+    return model.predict(text)
+
+def main():
+    html_temp = """
+    <div style="background-color:green;padding:10px;margin-bottom:20px;">
+    <h2 style="color:white;text-align:center;">Disaster Tweet Detection</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    st.text("Paste the text for prediction")
+
+    text = st.text_area("Text", "Type Here")
+    if text:
+        result = ""
+        if st.button("Predict"):
+            result = predict_the_output(text)
+            print(result)
+            if result[0] == 1:  # Adjust threshold based on your model
+                st.success('This is a Disaster Tweet')
+            else:
+                st.error('This is a Non Disaster Tweet')
+
+
+if __name__ == '__main__':
+    main()
